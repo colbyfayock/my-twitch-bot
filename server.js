878b7736fd846c3788c95fd218f185c32b4211b4
colbyfayock <fayock@gmail.com>
2021-03-21 23:38:44 -0400

    Step 1: installing tmi.js and listening to new messages on Twitch

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('Hello, Twitch!');
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+  connection: {
+    reconnect: true
+  },
+  channels: [
+    'spacejellybot'
+  ]
+});
+
+client.connect();
+
+client.on('message', async (channel, context, message) => {
+  console.log('channel', {
+    channel,
+    user: context.username,
+    message
+  });
+});

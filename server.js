8db0810b37d98aee02c148f7dc9932b295713f9a
colbyfayock <fayock@gmail.com>
2021-03-22 10:36:22 -0400

    Step 2: setting up tmi.js to listen to messages in a Twitch chat with a bot account

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const tmi = require('tmi.js');
 
 const client = new tmi.Client({
@@ -6,15 +8,19 @@ const client = new tmi.Client({
   },
   channels: [
     'spacejellybot'
-  ]
+  ],
+  identity: {
+    username: process.env.TWITCH_BOT_USERNAME,
+    password: process.env.TWITCH_OAUTH_TOKEN
+  }
 });
 
 client.connect();
 
 client.on('message', async (channel, context, message) => {
-  console.log('channel', {
-    channel,
-    user: context.username,
-    message
-  });
+  const isNotBot = context.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME.toLowerCase();
+
+  if ( isNotBot ) {
+    client.say(channel, `Responding to ${context.username} message: ${message}`);
+  }
 });
